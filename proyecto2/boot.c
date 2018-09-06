d754826aa154a0b019ee7ed5abf11c7782e920a6
PVillagran <pvillagran@SO1ADebian.sistemasoperativos1.umg.edu.gt>
2018-09-06 01:01:36 -0600

    +Color, +linea nueva, +comentarios

--- proyecto2/boot.c
+++ proyecto2/boot.c
@@ -1,59 +1,41 @@
-/*
-*  boot.c
-*/
+void clear();
 
 void kmain(void)
 {
+ const char *str = "1 Hola Mundo... \n Grupo Debian \n Coordinador: Pablo Villagran ";
+ char *vidptr = (char*)0xb8000; // array de caracteres en pantalla
+ unsigned int i = 0;
+ unsigned int j = 0;
+ unsigned int n = 1;
 
-	const char *str = " Hola Mundo... \n Grupo: Debian \n Cordinador: Pablo Villagran ";
+ clear();
 
-	char *vidptr = (char*)0xb8000; 	// la memoria de video inicia aqui.
-	
-        unsigned int i = 0;
+ /* Escritura de mensaje */
+ while(str[j] != '\0') {
+  if(str[j] == '\n')
+  {//si el ascii es 10 (nueva linea)
+   i = (80*2*n)-2; //agrega el ancho de la fila al cursor
+   ++n;
+  }
+  else
+  {
+   vidptr[i] = str[j];//Asigna valor de caracter
+   vidptr[i+1] = 0x0a;//Indica color de pantalla
+   i = i + 2;
+  }
+  ++j;
+ }
+ return;
+}
 
-	unsigned int j = 0;
+void clear(){
+ char *vidptr = (char*)0xb8000; // array de caracteres en pantalla
+ unsigned int j = 0;
+ /*Limpia la pantalla*/
+ while(j < 80 * 25 * 2) {
+  vidptr[j] = ' ';
+  vidptr[j+1] = 0x07;
+  j = j + 2;
+ }
+}
 
-	/* Este bucle limpia la pantalla
-	* 
-        there are 25 lines each of 80 columns; each element takes 2 bytes */
-	
-while(j < 80 * 25 * 2) {
-
-		/* blank character */
-	
-         	vidptr[j] = ' ';
-
-		/* attribute-byte - light grey on black screen */
-	
-        	vidptr[j+1] = 0x07; 		
-	
-        	j = j + 2;
-
- 	}
-
-	j = 0;
-
-	
-                /* this loop writes the string to video memory */
-	
-
-                while(str[j] != '\0') {
-		
-                /* the character's ascii */
-		
-		vidptr[i] = str[j];
-		
-		/* attribute-byte: give character black bg and light grey fg */
-
-		vidptr[i+1] = 0x07;
-		
-		++j;
-		
-		i = i + 2;
-	
-		}
-	
-		return;
-
-	}
- 
