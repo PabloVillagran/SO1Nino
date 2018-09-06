32b941800c7bd17d6ff50e7d52f4781c3f58bf94
PVillagran <pvillagran@SO1ADebian.sistemasoperativos1.umg.edu.gt>
2018-09-06 01:03:44 -0600

    comandos para compilar kernel, no toma en cuenta bootloader

---
+++ proyecto2/compilar.sh
@@ -0,0 +1,6 @@
+gcc -m32 -c boot.c -o bootc.o
+echo "Se ha compilado el kernel en C"
+ld -m elf_i386 -T link.ld -o ./kernel/boot/kernel boot.o bootc.o
+echo "Se han vinculado los archivos"
+grub-mkrescue -o boot.iso kernel
+echo "Se ha terminado la compilación del kernel"
