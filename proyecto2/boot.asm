ebb14ebeb4c85d2b7a2e13d3547d7c4ec5a917d6
PVillagran <pvillagran@SO1ADebian.sistemasoperativos1.umg.edu.gt>
2018-09-06 22:14:05 -0600

    +comentarios en bootloader

--- proyecto2/boot.asm
+++ proyecto2/boot.asm
@@ -1,37 +1,24 @@
- 
-;;kernel.asm
 
-bits 32			
-;nasm directive - 32 bit
-section .text
-	;multiboot spec
-        
-      align 4
-  
-      dd 0x1BADB002            ;magic
-    
-      dd 0x00                  ;flags
-
+bits 32	        ;indica a nasm que se debe compilar en 32 bits
+section .text	;esta seccion delimita banderas para el booteo en GRUB
+      align 4   ;esta bandera indica un 'formato' de 4 espacios al archivo compilado
+      dd 0x1BADB002 ;numero que indica a GRUB donde bootear
+      dd 0x00   ;indica a GRUB que todos los modulos de booteo deben estar alineados
+                ;en un limite de 4KB
       dd - (0x1BADB002 + 0x00) ;checksum. m+f+c should be zero
+                ;realiza la operacion sobre los ultimos valores
 
 global start
 
-extern kmain	 
-      ;kmain is defined in the c file
+extern kmain	;funcion kmain definida en el archivo c
 
 start:
+                ;funcion principal
+      cli       ;cambia una bandera interna para el bloqueo de interrupciones
+      mov esp, stack_space ;asigna puntero para pila de instrucciones
+      call kmain ;llama al kernel
+      hlt       ;permite interrupciones
 
-      cli 			
-      ;block interrupts
-  
-      mov esp, stack_space	
-      ;set stack pointer
-  
-      call kmain
-  hlt
-      ;halt the CPU
-
-
-      section .bss
-stack_space: resb 8192		
-      ;8KB for stack;;
+section .bss
+      ;seccion de bits vacios a manera de reservar memoria.
+stack_space: resb 8192 ;reserva 8KB para la pila de instrucciones
